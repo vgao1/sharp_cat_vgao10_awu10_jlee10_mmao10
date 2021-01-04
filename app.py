8b918ad064f2a5a1dc4dcb12e42326e205b4fa34
alvin-wu03 <60025632+alvin-wu03@users.noreply.github.com>
2021-01-04 17:23:47 -0500

    Added signup, login, sessions, user database

--- app.py
+++ app.py
@@ -7,7 +7,14 @@ from flask import Flask,session            #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
 import os
+import sqlite3   #enable control of an sqlite database
 
+DB_FILE="discobandit.db"
+db = sqlite3.connect(DB_FILE, check_same_thread = False) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
+c.execute('CREATE TABLE IF NOT EXISTS users(Username text, Password text, Bio text);')
+username = ''
+password = ''
 #the conventional way:
 #from flask import Flask, render_template, request
 
@@ -15,13 +22,13 @@ app = Flask(__name__)    #create Flask object
 app.secret_key = os.urandom(24)
 
 # delete after creating username/password in /auth
-username = "dullcat" #USERNAME
-password = "sharpiecats" #PASSWORD
+#username = "dullcat" #USERNAME
+#password = "sharpiecats" #PASSWORD
 
 @app.route("/") #, methods=['GET', 'POST'])
 def disp_loginpage():
     if 'username' in session:
-        return render_template('response.html', user = username, method = request.method)
+        return render_template('response.html', user = username)
     return render_template('login.html')
 
 # (a/j) needs to be done 
@@ -30,8 +37,7 @@ def disp_loginpage():
 def authenticate():
     problem = 'none'
     if request.args['username'] == '' or request.args['password'] == '': #Check if fields are filled
-        problem = 'Some fields are empty, try again'
-        return render_template('error.html', error = problem)     
+        return render_template('error.html', error = 'Some fields are empty, try again')     
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
     print(app)
@@ -43,25 +49,42 @@ def authenticate():
     print(request.args['username'])
     print("***DIAG: request.headers ***")
     print(request.headers)
-    if username == request.args['username'] and password == request.args['password']: #Verify username and password
+    username = request.args['username']
+    password = request.args['password']
+    c = db.cursor()
+    c.execute('SELECT * FROM users WHERE username=? AND password = ?', (username,password))
+    data = c.fetchall()
+    if len(data) == 1:
         session['username'] = username
-        return render_template ('response.html', user = username, method = request.method)
+        session['password'] = password
+        return render_template('response.html', user = request.args['username'])
     else:
-        if username == request.args['username']: #Incorrect password
-            problem = 'Incorrect Password'
-        else:
-            if password == request.args['password']: #Incorrect username
-                problem = 'Incorrect Username'
-            else:
-                problem = 'None of your information is correct' #Both incorrect
-        return render_template ('error.html', error = problem)
+        return render_template ('error.html')
 
 # (a/j) needs to be done 
 # sign up for an account, signup.html takes username, password, bio 
 # check if username is unique, add password specifications if desired
-@app.route("/newuser") 
+
+@app.route("/signup") #methods = ['GET','POST'])
+def signup():
+        c = db.cursor()
+        username = request.args['newusername']
+        password = request.args['newpassword']
+        bio = request.args['bio']
+        params = (username,password,bio)
+        c.execute('SELECT * FROM users WHERE username=?', (username,))
+        data = c.fetchall()
+        if len(data) > 0:
+            return render_template('error.html', error = 'A user with that username already exists')
+        c.execute('INSERT INTO users(Username,Password,Bio) VALUES(?,?,?)', params)
+        db.commit()
+        return render_template('login.html')
+
+# middle method, going straight to signup doesn't work. renders the actual signup page    
+@app.route("/newuser", methods = ['GET','POST'])
 def newuser():
-    return render_template('signup.html')
+        return render_template('signup.html')
+
 
 # (a/j) needs to be done 
 # adds a blog, addblog.html(not completed) will take take in a title and a body of text.
